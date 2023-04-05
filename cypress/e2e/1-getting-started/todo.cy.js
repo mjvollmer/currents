282eb9c6a96c6ec7dfa1dd53cb7ff7c29ac1afcd
Matt Vollmer <mattv@cypress.io>
2023-04-05 12:47:45 -0400

    fix test

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -116,7 +116,7 @@ describe('example to-do app', () => {
       cy.get('.todo-list li')
         .should('have.length', 1)
         .first()
-        .should('have.text', 'Pay electricc bill')
+        .should('have.text', 'Pay electric bill')
 
       cy.contains('Walk the dog').should('not.exist')
     })
